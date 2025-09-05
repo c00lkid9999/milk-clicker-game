bb9e96b195831c66e7f5e9991b752fe58895a40d
c00lkid9999 <isaac.wicks@icloud.com>
2025-09-05 14:29:45 -0600

    Added vars

---
+++ main.js
@@ -0,0 +1,22 @@
+//vars
+
+//buildings
+let cows = 0;
+let farmhands = 0;
+let barns = 0;
+let milkmaids = 0;
+let cheesePoints = 0;
+
+//building costs
+let cowCost = 10;
+let farmhandCost = 10;
+let barnCost = 100;
+let milkmaidCost = 100;
+let cheeseCost = 500;
+
+//game fundamentals
+let milk = 0;
+let milkPerClick = 1;
+let milkPerSecond = 0;
+let clicks = 0;
+let growthRate = 1.1;
